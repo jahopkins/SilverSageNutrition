99c24b6f516710391e47fe6a0d7423309527522b
Jacqueline Hopkins <318jlh@gmail.com>
2023-02-13 20:19:45 -0800

    Set up

---
+++ Silver_Sage_NutritionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Silver_Sage_NutritionApp.swift
+//  Silver Sage Nutrition
+//
+//  Created by Jacqueline Hopkins on 2/13/23.
+//
+
+import SwiftUI
+
+@main
+struct Silver_Sage_NutritionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
